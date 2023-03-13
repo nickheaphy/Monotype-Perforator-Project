60e607770e86d5fa9db17d3e4b6bdc72521df021
Nick Heaphy <nick@nicnak.net>
2023-03-13 13:46:40 +1300

    Standard case character quantites

---
+++ Tape Generator/web/js/founttext.js
@@ -0,0 +1,89 @@
+var founttext = [
+    {
+        "desc" : "Capitals, Punctuation, Numbers - Jobbing+5",
+        "text" : "A".repeat(28+5) +
+                "B".repeat(10+5) +
+                "C".repeat(14+5) +
+                "D".repeat(16+5) +
+                "E".repeat(44+5) +
+                "F".repeat(10+5) +
+                "G".repeat(10+5) +
+                "H".repeat(20+5) +
+                "I".repeat(28+5) +
+                "J".repeat(6+5) +
+                "K".repeat(6+5) +
+                "L".repeat(16+5) +
+                "M".repeat(14+5) +
+                "N".repeat(24+5) +
+                "O".repeat(24+5) +
+                "P".repeat(10+5) +
+                "Q".repeat(4+5) +
+                "R".repeat(22+5) +
+                "S".repeat(26+5) +
+                "T".repeat(32+5) +
+                "U".repeat(14+5) +
+                "V".repeat(6+5) +
+                "W".repeat(10+5) +
+                "X".repeat(4+5) +
+                "Y".repeat(8+5) +
+                "Z".repeat(4+5) +
+                "&".repeat(8+5) +
+                "$".repeat(12+5) +
+                "1".repeat(20+5) +
+                "2".repeat(12+5) +
+                "3".repeat(12+5) +
+                "4".repeat(8+5) +
+                "5".repeat(8+5) +
+                "6".repeat(8+5) +
+                "7".repeat(8+5) +
+                "8".repeat(8+5) +
+                "9".repeat(18+5) +
+                "0".repeat(24+5) +
+                ".".repeat(40+5) +
+                ",".repeat(40+5) +
+                ";".repeat(12+5) +
+                ":".repeat(6+5) +
+                "-".repeat(10+5) +
+                "‘".repeat(20+5) + //opening (left) quotation mark U+2018
+                "!".repeat(4+5) +
+                "?".repeat(6+5) +
+                "(".repeat(18+5) +
+                "[".repeat(8+5) +
+                "ﬀ".repeat(6+5) +
+                "ﬁ".repeat(8+5) +
+                "ﬂ".repeat(6+5) +
+                "ﬃ".repeat(6+5) +
+                "ﬄ".repeat(6+5) +
+                "’".repeat(40+5) + //closing (right) quotation mark (apostrophe) U+2019
+                "_".repeat(8+5)
+    },
+    {
+        "desc" : "Lower Case - Jobbing+5",
+        "text" : "a".repeat(74+5) +
+                 "b".repeat(18+5) +
+                 "c".repeat(34+5) +
+                 "d".repeat(42+5) +
+                 "e".repeat(118+5) +
+                 "f".repeat(24+5) +
+                 "g".repeat(18+5) +
+                 "h".repeat(50+5) +
+                 "i".repeat(74+5) +
+                 "j".repeat(6+5) +
+                 "k".repeat(8+5) +
+                 "l".repeat(42+5) +
+                 "m".repeat(26+5) +
+                 "n".repeat(66+5) +
+                 "o".repeat(66+5) +
+                 "p".repeat(20+5) +
+                 "q".repeat(6+5) +
+                 "r".repeat(58+5) +
+                 "s".repeat(66+5) +
+                 "t".repeat(84+5) +
+                 "u".repeat(38+5) +
+                 "v".repeat(12+5) +
+                 "w".repeat(20+5) +
+                 "x".repeat(6+5) +
+                 "y".repeat(12+5) +
+                 "z".repeat(4+5)
+    }
+];
